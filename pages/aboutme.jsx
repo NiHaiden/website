889d1ba9aa4ec14d07da66a5d205e4cb34444a4a
Niklas <40402114+NiHaiden@users.noreply.github.com>
2022-10-31 12:28:36 +0000

    fix quoting

--- pages/aboutme.jsx
+++ pages/aboutme.jsx
@@ -8,7 +8,7 @@ export default function AboutMe() {
       <div className={styleAboutMe.container}>
         <div className={styleAboutMe.aboutmetext}>Hello, I am Niklas Haiden, currently living near Vienna in the beautiful country of Austria. 
         Since my early childhood, I am interest in computers and all that has to do with it. In June 2022 I received my high school diploma from the department of Informatics at HTL St.Pölten.
-        I am currently doing my mandatory service, also called "Zivildienst". When I am not in front of the computer, I enjoy travelling around, visiting new places and taking pictures with my camera or phone. I also enjoy cycling. </div>
+        I am currently doing my mandatory service, also called &quot;Zivildienst&quot;. When I am not in front of the computer, I enjoy travelling around, visiting new places and taking pictures with my camera or phone. I also enjoy cycling. </div>
         <div className={styleAboutMe.aboutmeimage}></div>
       </div>
     </div>
