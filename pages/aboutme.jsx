4eeb7facded16391fed38d22782d0de1378f8ecf
Niklas Haiden <niklas.haiden@gmail.com>
2022-11-09 09:52:01 +0000

    adjust image to right size

--- pages/aboutme.jsx
+++ pages/aboutme.jsx
@@ -7,22 +7,22 @@ export default function AboutMe() {
     <div className={styles.bgimg}>
       <Navbar></Navbar>
       <div className={styleAboutMe.centerContainer}>
-      <div className={styleAboutMe.container}>
-        <div className={styleAboutMe.aboutmetext}>
-          Hello, I am Niklas Haiden, currently living near Vienna in the
-          beautiful country of Austria. Since my early childhood, I am interest
-          in computers and all that has to do with it. In June 2022 I received
-          my high school diploma from the department of Informatics at HTL
-          St.Pölten. I am currently doing my mandatory service, also called
-          &quot;Zivildienst&quot;. When I am not in front of the computer, I
-          enjoy travelling around, visiting new places and taking pictures with
-          my camera or phone. I also enjoy cycling.{" "}
-        </div>
+        <div className={styleAboutMe.container}>
+          <div className={styleAboutMe.aboutmetext}>
+            Hello, I am Niklas Haiden, currently living near Vienna in the
+            beautiful country of Austria. Since my early childhood, I am interest
+            in computers and all that has to do with it. In June 2022 I received
+            my high school diploma from the department of Informatics at HTL
+            St.Pölten. I am currently doing my mandatory service, also called
+            &quot;Zivildienst&quot;. When I am not in front of the computer, I
+            enjoy travelling around, visiting new places and taking pictures with
+            my camera or phone. I also enjoy cycling.{" "}
+          </div>
 
-        <div className={styleAboutMe.aboutmeimage}>
-          <Image src={"/me.JPG"} alt="a picture of myself" style={styleAboutMe.imageStyle} layout={"fill"}/>
+          <div className={styleAboutMe.aboutmeimage}>
+            <Image src={"/me.JPG"} alt="a picture of myself" style={styleAboutMe.imageStyle} width={"400"} height={500} />
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
