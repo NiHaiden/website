d1cf199bd2a29fb62e6e6abbe515f7076662e515
Niklas Haiden <niklas.haiden@gmail.com>
2023-08-15 13:53:37 +0000

    fix wrong order of elements in aboutme

--- pages/aboutme.jsx
+++ pages/aboutme.jsx
@@ -15,7 +15,22 @@ export default function AboutMe() {
 
             <div className={"flex flex-col flex-grow justify-center items-center px-10 h-full text-black"}>
                 <div className="w-4/5 max-w-3xl md:w-1/2 aboutmecontainer">
-                    <div className="workandedu">
+                   
+
+                    <div
+                        className="flex flex-col justify-center items-center p-10 text-white rounded-2xl shadow drop-shadow-2xl shortaboutme gradientbackground">
+                        <h1 className={"text-6xl font-black"}>Sitting in front of a screen since the beginning of time.</h1><br/>
+
+                        <p className={"text-lg"}>Since the beginning of my prime I have been interested in computers,
+                            from sending my first email at the age of 5 and destroying functioning OS installs to
+                            attending an IT vocational school from 2017-2022.
+                            My interests reach far and wide, from the field of DevOps to, especially, programming
+                            various things in different languages.
+                            When I&#39;m not consuming blue light in front of a screen, I&#39;m out and about, I
+                            especially like to visit various places and take photos of them.
+                            Feel free to browse around my various profiles and let&#39;s connect on Linkedin!</p>
+                    </div>
+                    <div className="mt-10 workandedu">
                         <div className={"mb-3 text-7xl font-black gradienttext"}>Work and Education</div>
                         <div
                             className={"grid grid-cols-1 grid-rows-3 gap-10 md:grid-cols-1 md:grid-rows-1 lg:grid-cols-2 lg:grid-rows-1 xl:grid-cols-2 xl:grid-rows-2 2xl:grid-cols-3 2xl:grid-rows-1"}>
@@ -35,20 +50,6 @@ export default function AboutMe() {
                                 <p className={"text-xl"}>2023-present</p>
                             </div>
                         </div>
-                    </div>
-
-                    <div
-                        className="flex flex-col justify-center items-center p-10 text-white rounded-2xl shadow drop-shadow-2xl shortaboutme gradientbackground">
-                        <h1 className={"text-6xl font-black"}>Sitting in front of a screen since the beginning of time.</h1><br/>
-
-                        <p className={"text-lg"}>Since the beginning of my prime I have been interested in computers,
-                            from sending my first email at the age of 5 and destroying functioning OS installs to
-                            attending an IT vocational school from 2017-2022.
-                            My interests reach far and wide, from the field of DevOps to, especially, programming
-                            various things in different languages.
-                            When I&#39;m not consuming blue light in front of a screen, I&#39;m out and about, I
-                            especially like to visit various places and take photos of them.
-                            Feel free to browse around my various profiles and let&#39;s connect on Linkedin!</p>
                     </div>
                 </div>
             </div>
